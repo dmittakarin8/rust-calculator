8b61785c3a1121d8fbaf52abdd284758f7795a05
Denis Mittakarin <dmittakarin8@hotmail.com>
2022-11-27 07:28:55 -0700

    first commit for a simple calculator in rust

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+//simple calculator that requests 2 numbers and an operation from the user
+use std::io::{stdin,stdout,Write};
+
+//try using a vec to store operation types
+
+fn read(input: &mut String){
+    stdout().flush()
+        .expect("failed to flush");
+    stdin().read_line(input)
+        .expect("failed to read");
+}
+
+fn main() {
+    println!("This is a calculator");
+
+    let mut num1 = String::new();
+    let mut num2 = String::new();
+    let mut operator = String::new();
+
+    print!("Please provide the first number ");
+    read(&mut num1);
+    print!("Please provide the second number ");
+    read(&mut num2);
+    print!("Please provide the operation you would like to perform ");
+    read(&mut operator);
+
+    let num1: f32 = num1.trim().parse().unwrap();
+    let num2: f32 = num2.trim().parse().unwrap();
+    let operator: char = operator.trim().chars().next().unwrap();
+
+    let operators = String::from("+-*/");
+
+    if !operators.contains(operator){
+        println!("invalid operation");
+        return;
+    }
+
+    println!("{}, {}, {} ", num1, num2, operator)
+
+    
+}
